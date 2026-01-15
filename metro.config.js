172123aea6b0c48dff7db2a201ed695884e65368
creativegoldmine <creativegoldmine@gmail.com>
2026-01-15 12:45:10 -0800

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,31 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer = {
+  ...config.transformer,
+  minifierConfig: {
+    keep_classnames: true,
+    keep_fnames: true,
+    mangle: {
+      keep_classnames: true,
+      keep_fnames: true,
+    },
+  },
+};
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'mjs', 'cjs'],
+};
+
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = config;
