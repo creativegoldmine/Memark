62e84de1523d4c2a2949dd4e7429c63cb9ac77c9
creativegoldmine <creativegoldmine@gmail.com>
2026-01-21 00:54:01 -0800

    Updated LogoHeader.tsx

--- components/LogoHeader.tsx
+++ components/LogoHeader.tsx
@@ -36,7 +36,7 @@ export function LogoHeader() {
 const styles = StyleSheet.create({
   header: {
     height: 60,
-    width: 0,
+    width: 100%,
     paddingHorizontal: 4,
     borderBottomWidth: 1,
     flexDirection: 'row',
